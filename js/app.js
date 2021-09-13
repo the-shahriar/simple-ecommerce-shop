999aa6f9ebbd1b62272a6d9473599f60c91ed374
the-shahriar <theshahriarislam@gmail.com>
2021-09-13 07:07:49 +0600

    rating info and total price bug fixed

--- js/app.js
+++ js/app.js
@@ -20,6 +20,7 @@ const showProducts = (products) => {
         </div>
         <h3>${product.title}</h3>
         <p>Category: ${product.category}</p>
+        <p><span class="product-rating">Rating: ${product.rating.rate}</span> Rating Count: ${product.rating.count}</p>
         <h2>Price: $ ${product.price}</h2>
         <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
         <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -33,6 +34,7 @@ const addToCart = (id, price) => {
     updatePrice("price", price);
 
     updateTaxAndCharge();
+    updateTotal();
     document.getElementById("total-Products").innerText = count;
 };
 
